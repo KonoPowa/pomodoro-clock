c955c84225a3d9dc1311c765679296c7eac09334
Yacob Abusadah <yacob.abusadah@gmail.com>
2020-09-04 17:03:07 -0400

    Updates

---
+++ precache-manifest.58b46ca51d517c431ba91338f744f36b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0211dab717270fdc5a67369b54f61ff",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "9f9f28cc22411fcaefbe",
+    "url": "/pomodoro-clock/static/css/main.6355d443.chunk.css"
+  },
+  {
+    "revision": "91ce6abbd89fc3cb8ba8",
+    "url": "/pomodoro-clock/static/js/2.134802b7.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/pomodoro-clock/static/js/2.134802b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f9f28cc22411fcaefbe",
+    "url": "/pomodoro-clock/static/js/main.94ff97c5.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  },
+  {
+    "revision": "c9d839e116850fa655c1c6922889f9e8",
+    "url": "/pomodoro-clock/static/media/Clock-alarm-electronic-beep.c9d839e1.mp3"
+  },
+  {
+    "revision": "9501b1fff8dc9a4a6e203550f7ecf287",
+    "url": "/pomodoro-clock/static/media/pomo.9501b1ff.png"
+  },
+  {
+    "revision": "75612c30e3149171dc1bc30f5a6f18d1",
+    "url": "/pomodoro-clock/static/media/pomo2.75612c30.png"
+  },
+  {
+    "revision": "56aeee64541fa688e2d94643d5a6885e",
+    "url": "/pomodoro-clock/static/media/pomo3.56aeee64.png"
+  }
+]);
